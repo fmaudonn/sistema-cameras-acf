d034d81aa6a79a06f4a21200d4508d70ad380d25
Fernando Maudonnet <fmaudonnet@gmail.com>
2026-06-23 21:42:36 -0300

    Change initial sidebar state to expanded

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(
     page_title="ACF Command Center",
     page_icon="🛡️",
     layout="wide",
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="expanded"
 )
 
 DATABASE_URL = st.secrets["DATABASE_URL"]
